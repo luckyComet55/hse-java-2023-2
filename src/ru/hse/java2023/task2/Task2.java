fd1325adba9735490eecdaf4a5287c68f1e88824
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-03-01 11:49:45 +0300

    Reformatted a bit

--- src/ru/hse/java2023/task2/Task2.java
+++ src/ru/hse/java2023/task2/Task2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Task2 {
   
-  public static int CompleteCycle() {
+  public static void main (String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("Enter absolute path to file to be read and then the one to be wrote at:");
     String filenameIn, filenameOut;
@@ -15,10 +15,10 @@ public class Task2 {
       filenameOut = scan.nextLine();
     } catch (NoSuchElementException | IllegalStateException exception) {
       System.out.println("Something went wrong while reading line");
-      return -1;
+      return;
     }
     File fileIn = new File(filenameIn);
-    
+  
     int uppercase = 0, lowercase = 0;
   
     try (FileReader fileReader = new FileReader(fileIn)) {
@@ -32,25 +32,17 @@ public class Task2 {
       }
     } catch (IOException exc) {
       System.out.println("Error while reading file");
-      return -1;
+      return;
     }
   
     String out = String.format("Uppercase: %d\nLowercase: %d", uppercase, lowercase);
-    
+  
     File fileOut = new File(filenameOut);
     try (FileWriter fileWriter = new FileWriter(fileOut)) {
       fileWriter.write(out);
+      System.out.println("Successfully wrote to file " + filenameOut);
     } catch (IOException exception) {
       System.out.println("Error while writing to file");
-      return -1;
     }
-    
-    return 0;
-  }
-  public static void main (String[] args) {
-    if (CompleteCycle() < 0) {
-      System.out.println("No");
-    }
-    System.out.println("Yes");
   }
 }
