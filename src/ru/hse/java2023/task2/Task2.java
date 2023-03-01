3d0f50842b9b3372fd282f1dd26ebc7083018872
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-03-01 11:46:06 +0300

    Done with writing to file

--- src/ru/hse/java2023/task2/Task2.java
+++ src/ru/hse/java2023/task2/Task2.java
@@ -8,16 +8,16 @@ public class Task2 {
   
   public static int CompleteCycle() {
     Scanner scan = new Scanner(System.in);
-    System.out.println("Enter absolute path to file:");
-    String filename;
+    System.out.println("Enter absolute path to file to be read and then the one to be wrote at:");
+    String filenameIn, filenameOut;
     try {
-      filename = scan.nextLine();
+      filenameIn = scan.nextLine();
+      filenameOut = scan.nextLine();
     } catch (NoSuchElementException | IllegalStateException exception) {
       System.out.println("Something went wrong while reading line");
       return -1;
     }
-    System.out.println("filename = " + filename);
-    File fileIn = new File(filename);
+    File fileIn = new File(filenameIn);
     
     int uppercase = 0, lowercase = 0;
   
@@ -35,7 +35,16 @@ public class Task2 {
       return -1;
     }
   
-    System.out.println("uppercase = " + uppercase + "; lowercase = " + lowercase);
+    String out = String.format("Uppercase: %d\nLowercase: %d", uppercase, lowercase);
+    
+    File fileOut = new File(filenameOut);
+    try (FileWriter fileWriter = new FileWriter(fileOut)) {
+      fileWriter.write(out);
+    } catch (IOException exception) {
+      System.out.println("Error while writing to file");
+      return -1;
+    }
+    
     return 0;
   }
   public static void main (String[] args) {
